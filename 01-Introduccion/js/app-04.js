d2728b6cdf4cee97d8aed04e2bb2187272194ef2
Jhonatan <jandres.otalvarop@gmail.com>
2021-10-07 03:00:08 -0500

    Consola de JavaScript

---
+++ 01-Introduccion/js/app-04.js
@@ -0,0 +1,59 @@
+/*
+    ==================================
+    ===== Consola de JavaScript ======
+    ==================================
+===============================================
+ - En la consola se pueden crear variables
+ - Como por ejemplo:
+ 
+ const Hola = "Hola desde la consola.";
+
+ - Para llamar la variable Hola en la consola
+ simplemente se escribe el nombre de la variable,
+ en este caso: Hola.
+=================================================
+ - También se pueden crear arreglos
+ -----------------------------------
+ Su sintaxis es:
+
+ console.log([1, 2, 3, 4]);
+ -----------------------------------
+ - Se puede cambiar su formato...
+
+ console.table([1, 2, 3, 4]);
+
+ console.table se utiliza para crear una tabla en 
+ consola.
+==================================================
+ - Se pueden crear errores con lo siguiente
+ ------------------------------------------
+ console.error('error');
+
+*/
+
+console.error('error');
+
+/*
+ se pueden enviar advertencias
+ -----------------------------
+ console.warn('Advertencia');
+*/
+
+console.warn('Advertencia');
+
+/*
+ Se puede limpiar la consola
+ ---------------------------
+ console.clear();
+ ===========================
+
+ Se pueden crear objetos; un objeto se define como una variable con múltiples valores.
+ -----------------------
+ const objeto = {
+
+    nombre: "Jhonatan"
+ }
+
+
+*/
+
