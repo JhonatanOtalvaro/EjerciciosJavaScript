d10100aaef0036f658095854e9e3138d463f82ac
Jhonatan <jandres.otalvarop@gmail.com>
2021-10-07 03:08:27 -0500

    Consideraciones con el ;

---
+++ 01-Introduccion/js/app-05.js
@@ -0,0 +1,30 @@
+/*
+
+==================================================
+===== Consideraciones en JavaScript con el ; =====
+==================================================
+
+en JavaScript el ; no es obligatorio a no ser que se
+quiera terminar una línea de código.
+
+Por ejemplo:
+*/
+
+console.log("Hola")
+console.log("Mundo")
+
+/*
+
+== Nota ==
+No se pueden colocar dos ordenes directas a Javascript una junto a la otra
+así:
+
+console.log("Hola") console.log("Mundo")
+
+a no ser que se le ponga ; a la primer orden.
+
+console.log("Hola"); console.log("Mundo")
+
+aunque eso es una mala práctica.
+
+*/
